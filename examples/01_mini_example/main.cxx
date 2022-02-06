10694e5106651ceaebf8d100fdd8898616f782df
smalldy <743682730@qq.com>
2022-02-06 16:59:42 +0800

    为示例1添加合适的注释

--- examples/01_mini_example/main.cxx
+++ examples/01_mini_example/main.cxx
@@ -1,6 +1,6 @@
 #include "easy_msg.h"
 #include <iostream>
-
+//项目中需要传递的结构体
 struct SomeMsg {
   std::string msg_str;
 };
@@ -9,14 +9,17 @@ struct SomeMsg2 {
   std::string msg_str2;
 };
 
-// reg struct to msg
+// 将其注册为消息
 EASY_MSG_DEFINE(SomeMsg, SomeMsgId);
 
 int main() {
-  // define a msg var
+  // 填充消息字段
   SomeMsgId::MsgType msg;
   msg.msg_str = "ada";
-  // match msg
+
+  //.....收到消息后....
+
+  // 进行消息匹配测试
   if (msg.match<SomeMsgId>()) {
     std::cout << "msg matched = " << msg.id() << std::endl;
   }
